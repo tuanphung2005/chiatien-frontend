d3cf248bf34491b01e9b3b55708d49fe90238989
TUAN <tuan29102005@gmail.com>
2026-01-15 15:09:21 +0700

    base

--- app/index.tsx
+++ app/index.tsx
@@ -9,55 +9,90 @@ import { Button, ButtonText } from '@/components/ui/button';
 import { useRouter } from 'expo-router';
 import { Icon } from '@/components/ui/icon';
 
+import {
+    AlertDialog,
+    AlertDialogContent,
+    AlertDialogHeader,
+    AlertDialogBody,
+    AlertDialogBackdrop,
+    AlertDialogFooter,
+    AlertDialogCloseButton
+} from '@/components/ui/alert-dialog';
+import { Heading } from '@/components/ui/heading';
+
 const FeatureCard = ({ iconSvg: IconSvg, name, desc }: any) => {
-  return (
-    <Box
-      className="flex-column md:flex-1 m-2 p-4 rounded-lg bg-background-0/40"
-      key={name}
-    >
-      <Box className="items-center flex flex-row">
-        <Icon as={IconSvg}/>
-        <Text className="font-medium ml-2 text-xl">{name}</Text>
-      </Box>
-      <Text className="mt-2">{desc}</Text>
-    </Box>
-  );
+    return (
+        <Box
+            className="flex-column md:flex-1 m-2 p-4 rounded-lg bg-background-0/40"
+            key={name}
+        >
+            <Box className="items-center flex flex-row">
+                <Icon as={IconSvg} />
+                <Text className="font-medium ml-2 text-xl">{name}</Text>
+            </Box>
+            <Text className="mt-2">{desc}</Text>
+        </Box>
+    );
 };
 
 export default function Home() {
-  const router = useRouter();
-  return (
-    <Box className="flex-1 bg-background-300 h-[100vh]">
-        <Box className="absolute h-[500px] w-[500px] lg:w-[700px] lg:h-[700px]">
-          <Gradient />
-        </Box>
-      {/* <ScrollView
+    const router = useRouter();
+    const [showAlertDialog, setShowAlertDialog] = React.useState(false);
+
+    const handleClose = () => setShowAlertDialog(false);
+
+
+
+    return (
+        <Box className="flex-1 bg-background-300 h-[100vh]">
+            <Box className="absolute h-[500px] w-[500px] lg:w-[700px] lg:h-[700px]">
+                <Gradient />
+            </Box>
+            {/* <ScrollView
         style={{ height: '100%' }}
         contentContainerStyle={{ flexGrow: 1 }}
       > */}
-        <Box className="flex flex-1 items-center mx-5 lg:my-24 lg:mx-32 py-safe">
-          <Box className="gap-10 base:flex-col sm:flex-row justify-between sm:w-[80%] md:flex-1">
-            <Box className="bg-background-template py-2 px-6 rounded-full items-center flex-column md:flex-row md:self-start">
-              <Text className="text-white font-medium">
-                Get started by editing
-              </Text>
-              <Text className="text-white font-medium ml-2">./App.tsx or ./app/index.tsx (or whatever entry point you have)</Text>
+            <Box className="flex flex-1 items-center mx-5 lg:my-24 lg:mx-32 py-safe">
+                <Box className="gap-10 base:flex-col sm:flex-row justify-between sm:w-[80%] md:flex-1">
+                    <Button
+                        size="md"
+                        className="bg-primary-500 px-6 py-2 rounded-full"
+                        onPress={() => {
+                            router.push('/tabs/tab1');
+                        }}
+                    >
+                        <ButtonText>Explore Tab Navigation</ButtonText>
+                    </Button>
+                </Box>
+                <Box className="flex-1 justify-center items-center h-[20px] w-[300px] lg:h-[160px] lg:w-[400px]">
+                    <Button onPress={() => setShowAlertDialog(true)}>
+                        <ButtonText>Show alert</ButtonText>
+                    </Button>
+
+                    <AlertDialog isOpen={showAlertDialog} onClose={handleClose} size ="md">
+                        <AlertDialogBackdrop />
+                        <AlertDialogContent>
+                            <AlertDialogHeader>
+                                <Heading>Title</Heading>
+                            </AlertDialogHeader>
+
+                            <AlertDialogBody>
+                                <Text>
+                                    this is a cool text
+                                </Text>
+                            </AlertDialogBody>
+
+                            <AlertDialogFooter>
+                                <Button onPress={handleClose}>
+                                    <ButtonText>Close</ButtonText>
+                                </Button>
+                            </AlertDialogFooter>
+                        </AlertDialogContent>
+                    </AlertDialog>
+
+                </Box>
             </Box>
-            <Button
-              size="md"
-              className="bg-primary-500 px-6 py-2 rounded-full"
-              onPress={() => {
-                router.push('/tabs/tab1');
-              }}
-            >
-              <ButtonText>Explore Tab Navigation</ButtonText>
-            </Button>
-          </Box>
-          <Box className="flex-1 justify-center items-center h-[20px] w-[300px] lg:h-[160px] lg:w-[400px]">
-            <Logo />
-          </Box>
+            {/* </ScrollView> */}
         </Box>
-      {/* </ScrollView> */}
-    </Box>
-  );
+    );
 }
